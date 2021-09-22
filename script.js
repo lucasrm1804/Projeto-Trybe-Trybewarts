3f22d3317fd4d089dd2d44f184a370ccb2ab1bbf
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-09-22 17:06:47 -0300

    requisito 3 correcao lint

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const buttonLogin = document.getElementById('buttonLogin');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 
-function buttonInput () {
+function buttonInput() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
-  }else {
+  } else {
     alert('Email ou senha inválidos.');
   }
 }
