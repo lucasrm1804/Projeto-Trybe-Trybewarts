7476a14b13e1944e3a2f8f2ae6346fca00944ad9
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-09-22 22:20:38 -0300

    concluido requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonLogin = document.getElementById('buttonLogin');
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
+const submitBtn = document.getElementById('submit-btn');
+const agreementCheckbox = document.getElementById('agreement');
 
 function buttonInput() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -11,3 +13,13 @@ function buttonInput() {
 }
 
 buttonLogin.addEventListener('click', buttonInput);
+
+function enableDisableButton() {
+  if (agreementCheckbox.checked) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+
+agreementCheckbox.addEventListener('click', enableDisableButton);
