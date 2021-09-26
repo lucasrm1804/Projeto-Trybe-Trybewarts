bad30f8b7481f2ecc77aff3f0b209e2f2ef75848
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-09-26 19:47:21 -0300

    concluido requisito 21

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const submitBtn = document.getElementById('submit-btn');
 const agreementCheckbox = document.getElementById('agreement');
 const counter = document.getElementById('counter');
 const textArea = document.getElementById('textarea');
+const form = document.getElementById('evaluation-form');
+
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const inputSelectorInput = document.getElementById('house');
+const inputFamily = form.family;
+
+const inputSubject = document.getElementsByClassName('subject');
+const inputRate = form.rate;
 
 function buttonInput() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -31,3 +41,36 @@ function counterHandler() {
 }
 
 textArea.addEventListener('keyup', counterHandler);
+
+document.body.addEventListener('click', (event) => {
+  console.dir(event.target);
+});
+
+console.log(inputName);
+
+inputName.addEventListener('input', () => {
+  console.log(inputName.value);
+});
+
+function makeSubjectStr() {
+  let subjectStr = '';
+  for (let index = 0; index < inputSubject.length; index += 1) {
+    const element = inputSubject[index];
+    if (element.checked !== false) {
+      subjectStr = `${subjectStr} ${element.value},`;
+    }
+  }
+  return subjectStr;
+}
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  form.innerText = `Nome: ${inputName.value} ${inputLastName.value} 
+  Email: ${inputEmail.value} 
+  Casa: ${inputSelectorInput.value}
+  Família: ${inputFamily.value} 
+  Matérias: ${makeSubjectStr()} 
+  Avaliação: ${inputRate.value} 
+  Observações: ${textArea.value} 
+  `;
+});
